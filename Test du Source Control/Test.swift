02c7bcb5fd373f30f44f02d7af01dc0b77ccf339
jonhyun <godaiys@hotmail.com>
2018-07-11 20:27:14 -0400

    ajout classe class

---
+++ Test du Source Control/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  Test du Source Control
+//
+//  Created by MyMac on 18-07-11.
+//  Copyright © 2018 Apex. All rights reserved.
+//
+
+import Foundation
+
+func salut() {
+    print("salut tout le monde")
+}
